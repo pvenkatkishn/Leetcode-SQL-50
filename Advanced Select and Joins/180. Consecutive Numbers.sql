ecaf77e8ba7b9b06fade471b220906ee6711f565
pvenkatkishn <vprasanthraaja@gmail.com>
2025-02-07 13:24:24 -0800

    Create 180. Consecutive Numbers.sql
    https://leetcode.com/problems/consecutive-numbers/?envType=study-plan-v2&envId=top-sql-50

---
+++ Advanced Select and Joins/180. Consecutive Numbers.sql
@@ -0,0 +1,23 @@
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+
+Solution:
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l2.id = l1.id + 1
+JOIN Logs l3 ON l3.id = l2.id + 1
+WHERE l1.num = l2.num AND l2.num= l3.num;
