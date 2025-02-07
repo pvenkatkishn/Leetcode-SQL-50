11235b03383ce16faf2d57cb4aa1349b0178702a
pvenkatkishn <vprasanthraaja@gmail.com>
2025-02-07 13:02:51 -0800

    Create 1045. Customers Who Bought All Products.sql
    https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50

---
+++ Sorting and Grouping/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,39 @@
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+This table may contain duplicates rows. 
+customer_id is not NULL.
+product_key is a foreign key (reference column) to Product table.
+ 
+
+Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key (column with unique values) for this table.
+ 
+
+Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+Return the result table in any order.
+
+
+
+Solution:
+WITH customer_counts AS (
+    SELECT customer_id, COUNT(DISTINCT product_key) AS product_count
+    FROM Customer
+    GROUP BY customer_id
+)
+SELECT customer_id
+FROM customer_counts
+WHERE product_count = (SELECT COUNT(*) FROM Product);
+
