72027f0688815d74a4a44d10cabb1496f16ba682
pvenkatkishn <vprasanthraaja@gmail.com>
2025-02-07 12:15:06 -0800

    Create 1174. Immediate Food Delivery II.sql
    https://leetcode.com/problems/immediate-food-delivery-ii/?envType=study-plan-v2&envId=top-sql-50

---
+++ Aggregate Functions/1174. Immediate Food Delivery II.sql
@@ -0,0 +1,28 @@
+Table: Delivery
+
++-----------------------------+---------+
+| Column Name                 | Type    |
++-----------------------------+---------+
+| delivery_id                 | int     |
+| customer_id                 | int     |
+| order_date                  | date    |
+| customer_pref_delivery_date | date    |
++-----------------------------+---------+
+delivery_id is the column of unique values of this table.
+The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+ 
+
+If the customers preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+
+The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
+Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+
+
+Solution:
+SELECT ROUND((COUNT(*) * 100)/(SELECT COUNT(DISTINCT customer_id) FROM delivery), 2) AS immediate_percentage
+FROM delivery
+WHERE (customer_id, customer_pref_delivery_date) IN(
+  SELECT customer_id, MIN(order_date)
+  FROM delivery
+  GROUP BY customer_id )
