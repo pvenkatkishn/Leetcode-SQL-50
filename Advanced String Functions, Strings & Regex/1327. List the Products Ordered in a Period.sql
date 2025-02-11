0351a85f0b14a96755979254fc7bb1482af30792
pvenkatkishn <vprasanthraaja@gmail.com>
2025-02-11 11:54:21 -0800

    Create 1327. List the Products Ordered in a Period.sql
    https://leetcode.com/problems/list-the-products-ordered-in-a-period/?envType=study-plan-v2&envId=top-sql-50

---
+++ Advanced String Functions, Strings & Regex/1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,40 @@
+Table: Products
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| product_id       | int     |
+| product_name     | varchar |
+| product_category | varchar |
++------------------+---------+
+product_id is the primary key (column with unique values) for this table.
+This table contains data about the companys products.
+ 
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| order_date    | date    |
+| unit          | int     |
++---------------+---------+
+This table may have duplicate rows.
+product_id is a foreign key (reference column) to the Products table.
+unit is the number of products ordered in order_date.
+ 
+
+Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+
+Return the result table in any order.
+
+
+
+Solution:
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM products p 
+JOIN Orders o ON o.product_id = p.product_id 
+AND o.order_date BETWEEN "2020-02-01" AND "2020-02-29" 
+GROUP BY p.product_name
+HAVING unit >= 100
